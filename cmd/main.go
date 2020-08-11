1c63f7d1266d4568c9fbf8b2ad7900154c39a12a
richard bright <rkbright1@gmail.com>
2020-08-11 04:39:55 -0700

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"log"
 	"server"
 )
 
 func main() {
 	r := server.NewRunner()
-	// r.InstallGem("bundler")
-	// r.InstallGem("jekyll")
-	err := r.Command("date")
-	if err != nil {
-		log.Fatal(err)
-	}
+	r.InstallGem("bundler")
+	r.InstallGem("jekyll")
 }
